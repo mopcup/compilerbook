21429f81c6839705fa1d4aaadfb71e465907f8c6
mopcup <mopcup.jp@gmail.com>
2023-12-13 02:08:52 +0900

    9cc.cを複数ファイルに分割

---
+++ 9cc.h
@@ -0,0 +1,84 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//
+// tokenize.c
+//
+
+// トークンの種類
+typedef enum {
+  TK_RESERVED,  // 記号
+  TK_NUM,       // 整数
+  TK_EOF,       // 入力の終わりを表すトークン
+} TokenKind;
+
+// トークン型
+typedef struct Token Token;
+struct Token {
+  TokenKind kind; // トークンの型
+  Token *next;    // 次の入力トークン
+  int val;        // kindがTK_NUMの場合、その数値
+  char *str;      // トークン文字列
+  int len;        // トークンの長さ
+};
+
+void error(char *fmt, ...);
+void error_at(char *loc, char *fmt, ...);
+bool consume(char *op);
+void expect(char *op);
+int expect_number();
+bool startswith(char *p, char *q);
+Token *new_token(TokenKind kind, Token *cur, char *str, int len);
+Token *tokenize();
+
+extern Token *token;
+extern char *user_input;
+
+//
+// parse.c
+//
+
+// 抽象構文木のノードの種類
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_EQ,  // ==
+  ND_NE,  // !=
+  ND_LT,  // <
+  ND_LE,  // <=
+  ND_NUM, // 整数
+} NodeKind;
+
+// ノード型
+typedef struct Node Node;
+struct Node {
+  NodeKind kind;  // ノードの型
+  Node *lhs;      // 左辺
+  Node *rhs;      // 右辺
+  int val;        // kindがND_NUMの場合のみ使う
+  char *str;      // トークン文字列
+  int len;        // トークンの長さ
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
+Node *mul();
+Node *unary();
+Node *primary();
+
+//
+// codegen.c
+//
+
+void gen(Node *node);
+void codegen(Node *node);
